4229719039dbe8a8b4bdb10d634e4501a680b5a2
Max Roger Morillo Alvarado <maxrogermorillo@gmail.com>
2022-01-21 02:19:36 -0300

    Adding chrome parameters for better compatibility with heroku

--- bot.py
+++ bot.py
@@ -59,6 +59,8 @@ def scrap_symbol_data(exchange, symbol):
     options = Options()
     options.headless = True
     options.binary_location = CHROME_PATH
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(
         options=options, service=web_service)
     driver.implicitly_wait(10)
